fix: logprobs stimmen jetzt

pu_two_step.py
@@ -17,7 +17,7 @@ from scipy import sparse
 import pickle
 
 from transformers import BasicPreprocessor, TextStats, FeatureNamePipeline
-from simple_nb import proba_label_MNB
+from proba_label_nb import proba_label_MNB
 
 
 # TODO: PROPERLY ADJUST PROBABILITIES INSTEAD OF LABELS
@@ -26,25 +26,19 @@ from simple_nb import proba_label_MNB
 # satisfied when ratio of positively labelled u in U is in [0.1, 0.5]
 # Alternative: some minimum positive ratio as stopping criterion
 # Alternative: yield (model, ratio) so caller can choose desired one
-def expectation_maximization(P, U, N=[], outpath=None, max_imbalance=1.5, max_pos_ratio=0.5, tolerance=0.05, text=True):
+def i_EM(P, U, N=[], outpath=None, max_imbalance=1.5, max_pos_ratio=0.5, tolerance=0.05, text=True):
     """EM algorithm for positive set P, unlabelled set U and (optional) negative set N
 
     iterate NB classifier with updated labels for unlabelled set (initially negative) until convergence
     if U is much larger than L=P+N, randomly samples to max_imbalance-fold of |L|"""
 
-    if N:
-        L = P + N
-        y_L = np.concatenate(np.array([[1., 0.]] * np.shape(P)[0]),
-                             np.array([[0., 1.]] * np.shape(N)[0]))
-    else:
-        L = P
-        y_L = np.array([[1., 0.]] * np.shape(P)[0])
+    L, y_L = make_L_yL(P, N)
 
     if np.shape(U)[0] > max_imbalance * np.shape(L)[0]:
         U = np.array(sample(list(U), int(max_imbalance * np.shape(L)[0])))
-
-    ynU = np.array([[0., 1.]] * np.shape(U)[0])
     ypU = np.array([[0., 1.]] * np.shape(U)[0])
+
+    # initialize iteration variables
     ypU_old = [[-1, -1]]
     iterations = 0
     model = None
@@ -77,18 +71,11 @@ def expectation_maximization(P, U, N=[], outpath=None, max_imbalance=1.5, max_po
             print("Acceptable ratio of positively labelled sentences in U is reached.")
             break
 
-    # Begin evaluation
-    print("Building for evaluation")
-    X_train, X_test, y_train, y_test = tts(np.concatenate((P, U)),
-                                           np.concatenate(([1 for x in P],
-                                                           [0 for x in U])),
-                                           test_size=0.2)
-    evalmodel = build_model(X_train, y_train, text=text)
 
     print("Classification Report:\n")
-
-    y_pred = evalmodel.predict(X_test)
-    print(clsr(y_test, y_pred))
+    y_pred = model.predict(np.concatenate((L, U)))
+    print(y_pred)
+    print(clsr([l[0] for l in y_L] + [0. for u in ypU], y_pred))
 
     print("Returning final model after", iterations, "refinement iterations")
 
@@ -100,6 +87,15 @@ def expectation_maximization(P, U, N=[], outpath=None, max_imbalance=1.5, max_po
 
     return model
 
+def make_L_yL(P, N):
+    if N:
+        L = P + N
+        y_L = np.concatenate(np.array([[1., 0.]] * np.shape(P)[0]),
+                             np.array([[0., 1.]] * np.shape(N)[0]))
+    else:
+        L = P
+        y_L = np.array([[1., 0.]] * np.shape(P)[0])
+    return L, y_L
 
 # ----------------------------------------------------------------
 # general model builder

test_nb.py
@@ -1,11 +1,10 @@
-from simple_nb import proba_label_MNB
+from proba_label_nb import proba_label_MNB
 from random import randint
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import classification_report as clsr
 import pu_two_step
 import numpy as np
 from scipy.sparse import csr_matrix
-
 
 print("\nNB test")
 print("-------\n")
@@ -42,7 +41,7 @@ p_mnb = proba_label_MNB()
 
 X = [[randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1), randint(0, 1)]
      for x in range(1000)]
-y = [1.0 if (x[0] and x[1]) or (x[2] and x[1]) else 0.0 for x in X]
+y = [1 if (x[0] and x[1]) or (x[2] and x[1]) else 0 for x in X]
 
 # X = csr_matrix(X)
 
@@ -68,10 +67,10 @@ print("\nPU NB I-EM test")
 print("---------------\n")
 
 featurenum = 8
-quarter = int(featurenum/4)
+quarter = int(featurenum / 4)
 
 P = [([randint(0, 4) for i in range(quarter)]
-      + [randint(1, 2) for i in range(2*quarter)]
+      + [randint(1, 2) for i in range(2 * quarter)]
       + [1] * quarter
       )
      for j in range(500)]
@@ -79,22 +78,25 @@ P = [([randint(0, 4) for i in range(quarter)]
 P = np.array([csr_matrix(p) for p in P])
 
 U = [([randint(0, 2)
-      for i in range(2*quarter)]
-      + ([0] * (2*quarter))
+       for i in range(2 * quarter)]
+      + ([0] * (2 * quarter))
       )
-     for j in range(1500)]
-U += [[0] * featurenum] * 500
+     for j in range(500)]
+U += [[randint(0, 2)
+       for i in range(featurenum)]
+      for j in range(500)]
 U += [([randint(0, 2)
-      for i in range(2*quarter)]
-      + [randint(0, 2)
-      for i in range(2*quarter)]
-      )
-     for j in range(100)]
+        for i in range(2 * quarter)]
+       + [randint(0, 2)
+          for i in range(2 * quarter)]
+       )
+      for j in range(100)]
 U += [([randint(0, 4) for i in range(quarter)]
-      + [randint(1, 2) for i in range(2*quarter)]
-      + [1] * quarter
-      )
-     for j in range(100)]
+       + [randint(1, 2) for i in range(2 * quarter)]
+       + [1] * quarter
+       )
+      for j in range(500)]
+U += [[0]*featurenum] * 400
 
 U = np.array([csr_matrix(u) for u in U])
 
@@ -103,4 +105,4 @@ print("done preparing")
 # print(P[0])
 # print(U[0])
 
-pu_two_step.expectation_maximization(P, U, max_pos_ratio=1, max_imbalance=10, tolerance=0.1, text=False)
+pu_two_step.i_EM(P, U, max_pos_ratio=2, max_imbalance=10, tolerance=0.1, text=False)