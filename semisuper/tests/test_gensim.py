143a77e2b21f0c60df0c2b1c99f20e1039865250
Martin Wackerbauer <nachne@nachne.com>
2017-11-15 10:27:09 +0100

    simple gensim test

---
+++ semisuper/tests/test_gensim.py
@@ -0,0 +1,38 @@
+from semisuper import loaders, transformers
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+import gensim
+from gensim import corpora
+
+civic, abstracts = loaders.sentences_civic_abstracts()
+
+corpus = civic + abstracts
+
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation)
+lemma = WordNetLemmatizer()
+
+
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+
+doc_clean = [clean(doc).split() for doc in corpus]
+
+# Creating the term dictionary of our courpus, where every unique term is assigned an index.
+dictionary = corpora.Dictionary(doc_clean)
+
+# Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+
+# Creating the object for LDA model using gensim library
+Lda = gensim.models.ldamodel.LdaModel
+
+# Running and Trainign LDA model on the document term matrix.
+ldamodel = Lda(doc_term_matrix, num_topics=2, id2word=dictionary, passes=50)
+
+print(ldamodel.print_topics(num_topics=-1, num_words=20))
